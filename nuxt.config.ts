ac51d5d26b98625a987cd8757b4a5d9cf86a0c53
jefersonbrluz <jefersonbrluz@gmail.com>
2025-10-08 09:19:20 -0300

    feat: integrar os módulos Tailwind CSS e nuxt-icon, atualizar nuxt.config e adicionar tailwind.css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+// ./nuxt.config.ts
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon'
+  ],
+  css: [
+    '~/assets/css/tailwind.css'
+  ]
 })
