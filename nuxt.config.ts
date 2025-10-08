109930b3dd606b737d3204857e1a1b44a418fae3
jefersonbrluz <jefersonbrluz@gmail.com>
2025-10-08 11:17:59 -0300

    feat: atualizar nuxt.config.ts para incluir estilos Vuetify e configurar definições de compilação; adicionar novas devDependencies para bibliotecas de ícones e Sass

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,19 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
-    '@nuxtjs/tailwindcss',
-    'nuxt-icon'
+    '@nuxtjs/tailwindcss'
   ],
   css: [
-    '~/assets/css/tailwind.css'
-  ]
+    '~/assets/css/tailwind.css',
+    'vuetify/lib/styles/main.sass',
+    '@mdi/font/css/materialdesignicons.min.css'
+  ],
+  build: {
+    transpile: ['vuetify']
+  },
+  vite: {
+    define: {
+      'process.env.DEBUG': false
+    }
+  }
 })
